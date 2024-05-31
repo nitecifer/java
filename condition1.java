cd96ec5c38cb9585063b739424dfd2f42226066d
nitecifer <nitegupta2004@gmail.com>
2024-05-31 23:17:15 +0530

    my code

---
+++ condition1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class condition1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if (x < 0) {
+            System.out.println("Negative");
+            
+        }else{
+            System.out.println("Postive");
+
+        }
+    }
+}

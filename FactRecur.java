8f0025ebdcc11c531e1e7ba2afdf64e609ffb331
nitecifer <nitegupta2004@gmail.com>
2024-06-21 00:52:32 +0530

    Recursive Fibo

---
+++ FactRecur.java
@@ -0,0 +1,16 @@
+public class FactRecur {
+    public static int calfactorial(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        int fact_nm1 = calfactorial(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+    public static void main(String args[]){
+        int n=6;
+        int ans = calfactorial(n);
+        System.out.println(ans);
+    }
+    
+}
---
+++ FibRecr.java
@@ -0,0 +1,21 @@
+public class FibRecr {
+    public static void printfib (int a , int b , int n){
+        if (n == 0) {
+            return;
+            
+        }
+        int c = a + b;
+        System.out.println(c);
+        printfib(b , c , n-1);
+    }
+        public static void main (String args[]){
+            int a = 0 , b = 1;
+            System.out.println(a);
+            System.out.println(b);
+            int n = 8;
+            printfib(a , b ,n-2);
+
+        
+    }
+    
+}

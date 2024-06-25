726c2e7a5c3b5834df6999eae5eef6ee510c0eed
Nitish Gupta <103733385+nitecifer@users.noreply.github.com>
2024-06-25 23:05:23 +0530

    Add files via upload

--- kadane.java
+++ kadane.java
@@ -12,6 +12,18 @@ public class kadane {
         }
         return mx;
     }
+    public static int kadaneAlgo(int a[],int n){
+        int mx=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            if(sum<0) sum=0;
+            else{
+                mx=Math.max(mx, sum);
+            }
+        }
+        return mx;
+    }
     public static void main(String[] args) {
         // int a[]={-2,-3,4,-1,-2,1,5,-3};
         int a[]={1,2,3,-2,5};

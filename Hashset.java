d5bb290876d7d29d5828cf94de1e6113818f06ec
nitecifer <nitegupta2004@gmail.com>
2024-06-29 23:54:49 +0530

    India India

---
+++ Hashset.java
@@ -0,0 +1,11 @@
+public class Hashset {
+    public static void Subsequences(String str , int idx , String newString, HashSet<String>){
+        
+        char curr = str.CharAt(idx);
+
+        // to be 
+        
+    }
+
+    
+}
---
+++ India.java
@@ -0,0 +1,7 @@
+public class India {
+    public static void main(String[] args) {
+        System.out.println("INDIA INDIA");
+        System.out.println("Rohit(Hitman) Sharma");
+        System.out.println("Virat(King) Kohli");
+    }
+}

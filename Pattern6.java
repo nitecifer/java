0ece3e10c05422a9fe90bd246be08a67910b4481
nitecifer <nitegupta2004@gmail.com>
2024-06-30 23:30:33 +0530

    Pattern

---
+++ Pattern6.java
@@ -0,0 +1,14 @@
+public class Pattern6 {
+    public static void main(String args[]){
+        int n = 2;
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=n ; j++){
+                System.out.println("*"+" ");
+            }
+
+        }
+            
+        
+    }
+    
+}

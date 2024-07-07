03a4be65df012b37ba09b0e02f3308e1c9389554
nitecifer <nitegupta2004@gmail.com>
2024-07-07 21:41:35 +0530

    Add2

---
+++ Add2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Add2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the first nubmer");
+        int num1= sc.nextInt();
+
+        
+        System.out.print("Enter the second number");
+        int num2= sc.nextInt();
+
+        System.out.println(num1 + "*" + num2 + "=" +num1*num2);
+    }
+    
+}

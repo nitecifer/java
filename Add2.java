8991e20d6c3483a06cee6f06dbfd69272b9679c8
nitecifer <nitegupta2004@gmail.com>
2024-07-07 21:52:27 +0530

    Exercise

--- Add2.java
+++ Add2.java
@@ -3,11 +3,10 @@ public class Add2 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter the first nubmer");
+        System.out.println("Enter the first nubmer");
         int num1= sc.nextInt();
-
         
-        System.out.print("Enter the second number");
+        System.out.println("Enter the second number");
         int num2= sc.nextInt();
 
         System.out.println(num1 + "*" + num2 + "=" +num1*num2);
---
+++ Exercise5.java
@@ -0,0 +1,12 @@
+public class Exercise5 {
+    public static void main(String args[]){
+        int a=125;
+        int b=24;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    
+}

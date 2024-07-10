32a8166219af69b3f419d775095b755ab043f655
nitecifer <nitegupta2004@gmail.com>
2024-07-10 16:17:58 +0530

    Average

---
+++ Exercice10.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Exercice10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number"); 
+        int num1 = sc.nextInt(); 
+        System.out.print("Enter the second number");
+        int  num2= sc.nextInt();
+        System.out.print("Enter the third number");
+        int num3= sc.nextInt();
+        System.out.print("Enter the fourth number");
+        int num4 = sc.nextInt();
+        System.out.print("Enter the fifth number");
+        int num5 = sc.nextInt();
+
+
+        System.out.println("Average of the number"+(num1+num2+num3+num4+num5)/5);
+
+
+        
+
+       
+    
+    }
+    
+}
